6d3dfb1096da31103e96931f74ae6fd899c23990
Lacey Rice <Ricecoder@users.noreply.github.com>
2015-09-01 18:51:57 -0400

    Added beginning code
    Added starting code.

--- BasicCalculator.js
+++ BasicCalculator.js
@@ -1,1 +1,11 @@
+console.assert(add(0, 0), === 0);
+console.assert(add(0, 1), === 1);
+console.assert(add(0, 2), === 2);
 
+/**
+ * @param {Number} A to add to B
+ * @param {Number} B to add to A
+ * @return {Number} sum of A and B
+ */
+function add(A, B){
+} // END add
