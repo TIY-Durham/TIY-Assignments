db56d9a5aa75ef79f3687b8f76022b8e14c56ae7
Lacey R <laceyrice@me.com>
2015-09-01 21:57:52 -0400

    Added multi and div

--- BasicCalculator.js
+++ BasicCalculator.js
@@ -137,3 +137,124 @@ console.assert(sub(5, 9) === 4);
  * @param {Number} B to add to A
  * @return {Number} sum of A and B
  */
+// End Subtraction
+
+function multiply(A, B){
+  return A * B;
+};
+
+console.assert(multiply(0, 0) === 0);
+console.assert(multiply(0, 1) === 0);
+console.assert(multiply(0, 2) === 0);
+console.assert(multiply(0, 3) === 0);
+console.assert(multiply(0, 4) === 0);
+console.assert(multiply(0, 5) === 0);
+console.assert(multiply(0, 6) === 0);
+console.assert(multiply(0, 7) === 0);
+console.assert(multiply(0, 8) === 0);
+console.assert(multiply(0, 9) === 0);
+
+console.assert(multiply(1, 1) === 0);
+console.assert(multiply(1, 2) === 2);
+console.assert(multiply(1, 3) === 3);
+console.assert(multiply(1, 4) === 4);
+console.assert(multiply(1, 5) === 5);
+console.assert(multiply(1, 6) === 6);
+console.assert(multiply(1, 7) === 7);
+console.assert(multiply(1, 8) === 8);
+console.assert(multiply(1, 9) === 9);
+
+console.assert(multiply(2, 1) === 2);
+console.assert(multiply(2, 2) === 4);
+console.assert(multiply(2, 3) === 6);
+console.assert(multiply(2, 4) === 8);
+console.assert(multiply(2, 5) === 10);
+console.assert(multiply(2, 6) === 12);
+console.assert(multiply(2, 7) === 14);
+console.assert(multiply(2, 8) === 16);
+console.assert(multiply(2, 9) === 18);
+
+console.assert(multiply(3, 1) === 3);
+console.assert(multiply(3, 2) === 6);
+console.assert(multiply(3, 3) === 9);
+console.assert(multiply(3, 4) === 12);
+console.assert(multiply(3, 5) === 15);
+console.assert(multiply(3, 6) === 18);
+console.assert(multiply(3, 7) === 21);
+console.assert(multiply(3, 8) === 24);
+console.assert(multiply(3, 9) === 27);
+
+console.assert(multiply(4, 1) === 4);
+console.assert(multiply(4, 2) === 8);
+console.assert(multiply(4, 3) === 12);
+console.assert(multiply(4, 4) === 16);
+console.assert(multiply(4, 5) === 20);
+console.assert(multiply(4, 6) === 24);
+console.assert(multiply(4, 7) === 28);
+console.assert(multiply(4, 8) === 32);
+console.assert(multiply(4, 9) === 36);
+
+console.assert(multiply(5, 1) === 5);
+console.assert(multiply(5, 2) === 10);
+console.assert(multiply(5, 3) === 15);
+console.assert(multiply(5, 4) === 20);
+console.assert(multiply(5, 5) === 25);
+console.assert(multiply(5, 6) === 30);
+console.assert(multiply(5, 7) === 35);
+console.assert(multiply(5, 8) === 40);
+console.assert(multiply(5, 9) === 45);
+/**
+ * @param {Number} A to add to B
+ * @param {Number} B to add to A
+ * @return {Number} sum of A and B
+ */
+// End multiply
+
+function divide(A, B){
+  return B / A;
+};
+
+console.assert(divide(0, 0) === infinity);
+console.assert(divide(0, 1) === 0);
+console.assert(divide(0, 2) === 0);
+console.assert(divide(0, 3) === 0);
+console.assert(divide(0, 4) === 0);
+console.assert(divide(0, 5) === 0);
+console.assert(divide(0, 6) === 0);
+console.assert(divide(0, 7) === 0);
+console.assert(divide(0, 8) === 0);
+console.assert(divide(0, 9) === 0);
+
+console.assert(divide(1, 1) === 1);
+console.assert(divide(1, 2) === 2);
+console.assert(divide(1, 3) === 3);
+console.assert(divide(1, 4) === 4);
+console.assert(divide(1, 5) === 5);
+console.assert(divide(1, 6) === 6);
+console.assert(divide(1, 7) === 7);
+console.assert(divide(1, 8) === 8);
+console.assert(divide(1, 9) === 9);
+
+console.assert(divide(2, 1) === 2);
+console.assert(divide(2, 2) === 1);
+
+
+console.assert(divide(3, 1) === 3);
+console.assert(divide(3, 2) === 1.5);
+console.assert(divide(3, 3) === 1);
+
+console.assert(divide(4, 1) === 4);
+console.assert(divide(4, 2) === 2);
+console.assert(divide(4, 4) === 1);
+
+console.assert(divide(5, 1) === 5);
+console.assert(divide(5, 2) === 2.5);
+console.assert(divide(5, 4) === 1.25);
+console.assert(divide(5, 5) === 1);
+
+/**
+ * @param {Number} A to add to B
+ * @param {Number} B to add to A
+ * @return {Number} sum of A and B
+ */
+// End divide
