20176854bba250a4fe509b59256dbea1ded16517
Lacey R <laceyrice@me.com>
2015-09-01 22:55:32 -0400

    Code that does not work

--- StringCalculator.js
+++ StringCalculator.js
@@ -3,8 +3,16 @@ var test = require('mocha').it,
 
 test('fromEnglish: "zero" through "nine"', function(){
   expect(fromEnglish).to.exist;
-  expect(fromEnglish("zero")).to.equal(0);
-  expect(fromEnglish("one")).to.equal(1);
+  expect(fromEnglish("zero") === 0).to.equal(0);
+  expect(fromEnglish("one") === 1).to.equal(1);
+  expect(fromEnglish("two") === 2).to.equal(2);
+  expect(fromEnglish("three") === 3).to.equal(3);
+  expect(fromEnglish("four") === 4).to.equal(4);
+  expect(fromEnglish("five") === 5).to.equal(5);
+  expect(fromEnglish("six") === 6).to.equal(6);
+  expect(fromEnglish("seven") === 7).to.equal(7);
+  expect(fromEnglish("eight")=== 8).to.equal(8);
+  expect(fromEnglish("nine") === 9).to.equal(9);
   // more tests here, please...
 }); // END test(fromEnglish)
 
@@ -13,14 +21,61 @@ test('fromEnglish: "zero" through "nine"', function(){
  * @return {Number} value corresponding to `word`
  */
 function fromEnglish(word){
+  if("zero") {
+    return 0;
+  };
+
+  if ("one") {
+    return 1;
+  };
+
+  if ("two") {
+    return 2;
+  };
+
+  if ("three") {
+    return 3;
+  };
+
+  if ("four") {
+    return 4;
+  };
+
+  if ("five") {
+    return 5;
+  };
+
+  if ("six") {
+    return 6;
+  };
+
+  if ("seven") {
+    return 7;
+  };
+
+  if ("eight") {
+    return 8;
+  };
+
+  if ("nine") {
+    return 9;
+  };
+
   // fill this in after writing tests...
 } // END fromEnglish
 
 test('plus: ("zero", "zero") through ("zero", "nine")', function(){
   expect(plus).to.exist;
-  expect(plus("zero", "zero")).to.equal(0);
-  expect(plus("zero", "one")).to.equal(1); // change just one thing!
-  expect(plus("zero", "two")).to.equal(FILL_ME_IN); // ha!
+  expect(plus("zero" + "zero")).to.equal(0);
+  expect(plus("zero" + "one")).to.equal(1); // change just one thing!
+  expect(plus("zero" + "two")).to.equal(2); // ha!
+  expect(plus("zero" + "three")).to.equal(3);
+  expect(plus("zero" + "four")).to.equal(4);
+  expect(plus("zero" + "five")).to.equal(5);
+  expect(plus("zero" + "six")).to.equal(6);
+  expect(plus("zero" + "seven")).to.equal(7); // change just one thing!
+  expect(plus("zero" + "eight")).to.equal(8);
+  expect(plus("zero" + "nine")).to.equal(9);
   // write more tests like those...
 }); // END test(plus)
 
