1f2eaf0cf7251d8232ffce7ca683c4dc3bc398ae
Lacey R <laceyrice@me.com>
2015-09-02 20:02:57 -0400

    Everything works up to the 5s

--- BasicCalculator.js
+++ BasicCalculator.js
@@ -1,4 +1,4 @@
-function add(A, B){
+/*function add(A, B){
   return A + B;
 };
 
@@ -62,17 +62,17 @@ console.assert(add(5, 6) === 11);
 console.assert(add(5, 7) === 12);
 console.assert(add(5, 8) === 13);
 console.assert(add(5, 9) === 14);
+*/
 /**
  * @param {Number} A to add to B
  * @param {Number} B to add to A
  * @return {Number} sum of A and B
  */
 // END add
-function sub(A, B){
+/*function sub(A, B){
   return B - A;
 };
 
-console.assert(sub(0, 0) === 0);
 console.assert(sub(0, 1) === 1);
 console.assert(sub(0, 2) === 2);
 console.assert(sub(0, 3) === 3);
@@ -132,6 +132,7 @@ console.assert(sub(5, 6) === 1);
 console.assert(sub(5, 7) === 2);
 console.assert(sub(5, 8) === 3);
 console.assert(sub(5, 9) === 4);
+*/
 /**
  * @param {Number} A to add to B
  * @param {Number} B to add to A
@@ -139,7 +140,9 @@ console.assert(sub(5, 9) === 4);
  */
 // End Subtraction
 
-function multiply(A, B){
+
+
+/*function multiply(A, B){
   return A * B;
 };
 
@@ -203,6 +206,7 @@ console.assert(multiply(5, 6) === 30);
 console.assert(multiply(5, 7) === 35);
 console.assert(multiply(5, 8) === 40);
 console.assert(multiply(5, 9) === 45);
+*/
 /**
  * @param {Number} A to add to B
  * @param {Number} B to add to A
@@ -210,20 +214,12 @@ console.assert(multiply(5, 9) === 45);
  */
 // End multiply
 
+
 function divide(A, B){
   return B / A;
 };
 
-console.assert(divide(0, 0) === infinity);
-console.assert(divide(0, 1) === 0);
-console.assert(divide(0, 2) === 0);
-console.assert(divide(0, 3) === 0);
-console.assert(divide(0, 4) === 0);
-console.assert(divide(0, 5) === 0);
-console.assert(divide(0, 6) === 0);
-console.assert(divide(0, 7) === 0);
-console.assert(divide(0, 8) === 0);
-console.assert(divide(0, 9) === 0);
+//Zero division produces infinity
 
 console.assert(divide(1, 1) === 1);
 console.assert(divide(1, 2) === 2);
@@ -235,19 +231,16 @@ console.assert(divide(1, 7) === 7);
 console.assert(divide(1, 8) === 8);
 console.assert(divide(1, 9) === 9);
 
-console.assert(divide(2, 1) === 2);
+console.assert(divide(2, 1) === 0.5);
 console.assert(divide(2, 2) === 1);
 
 
-console.assert(divide(3, 1) === 3);
 console.assert(divide(3, 2) === 1.5);
 console.assert(divide(3, 3) === 1);
 
-console.assert(divide(4, 1) === 4);
 console.assert(divide(4, 2) === 2);
 console.assert(divide(4, 4) === 1);
 
-console.assert(divide(5, 1) === 5);
 console.assert(divide(5, 2) === 2.5);
 console.assert(divide(5, 4) === 1.25);
 console.assert(divide(5, 5) === 1);
