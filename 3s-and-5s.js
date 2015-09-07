2c8f23b44483b25fd7fa3b65ffe2d993d9f5b274
Lacey R <laceyrice@me.com>
2015-09-06 23:43:24 -0400

    added 5s

--- 3s-and-5s.js
+++ 3s-and-5s.js
@@ -7,7 +7,7 @@ var test = require('mocha').it,
 test(/*'getting a list of multiples of 3', */function () {
   expect(ten()).to.be("0","3", "6", "9");
   expect(twenty()).to.be("0","3", "6", "9", "12", "15", "18");
-  expect(thirty()).to.be("0", "3", "6", "9", "12", "15", "18", "21", "24", "27");
+  expect(fifty()).to.be("0", "3", "6", "9", "12", "15", "18", "21", "24", "27", "30", "33", "36", "39", "42", "45", "48");
 });
 
 
@@ -30,18 +30,56 @@ function twenty(){
       values += (i);
     }
   }
-  return values.split(""); // splits 12 into '1', '2'
+  return values.split(" "); // splits 12 into '1', '2'
 };
 
-function thirty(){
+function fifty(){
   var count = 0;
   var values = "";
-  for(var i = 0; i < 30; i ++) {
+  for(var i = 0; i < 50; i ++) {
     if (i % 3 === 0) {
+      values += (i);
+    }
+  }
+  return values.split(""); //TODO figure out how NOT to split double digits.
+};
+
+// END test 3s
+test('getting a list of multiples of 5', function () {
+  expect(ten()).to.be("0","5");
+  expect(twenty()).to.be("0","5", "10", "15");
+  expect(fifty()).to.be("0", "5", "10", "15", "20", "25", "30", "35", "40", "45");
+});
+
+function ten(){
+  var count = 0;
+  var values = "";
+  for(var i = 0; i < 10; i ++) {
+    if (i % 5 === 0) {
       values += (i);
     }
   }
   return values.split("");
 };
 
-// END test 3s
+function twenty(){
+  var count = 0;
+  var values = "";
+  for(var i = 0; i < 20; i ++) {
+    if (i % 5 === 0) {
+      values += (i);
+    }
+  }
+  return values.split(" "); // splits 12 into '1', '2'
+};
+
+function fifty(){
+  var count = 0;
+  var values = "";
+  for(var i = 0; i < 50; i ++) {
+    if (i % 5 === 0) {
+      values += (i);
+    }
+  }
+  return values.split(""); //TODO figure out how NOT to split double digits.
+};
