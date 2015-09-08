b883ad4c6b74258aa9b8e0b37732394e03a6db43
Lacey R <laceyrice@me.com>
2015-09-07 21:41:50 -0400

    Refactored

--- 3s-and-5s.js
+++ 3s-and-5s.js
@@ -2,84 +2,49 @@ var test = require('mocha').it,
     expect = require('chai').expect, // Use `expect(X).to.be` et al
     assert = require('chai').assert;  // OR use `assert.equal` etc
 
+    function multiples(num, max)
+    {
+      var values = [];
+      for(var i = num; i < max; i = i + num) {
+          values.push(i);
+      }
+      return values;
+    };
 
+    function ten(){
+      return multiples(3, 10);
+    };
 
-test(/*'getting a list of multiples of 3', */function () {
-  expect(ten()).to.be("0","3", "6", "9");
-  expect(twenty()).to.be("0","3", "6", "9", "12", "15", "18");
-  expect(fifty()).to.be("0", "3", "6", "9", "12", "15", "18", "21", "24", "27", "30", "33", "36", "39", "42", "45", "48");
+    function twenty(){
+      return multiples(3, 20);
+    };
+
+    function fifty(){
+      return multiples(3, 50);
+    };
+
+test('getting a list of multiples of 3', function () {
+  expect(ten()).to.deep.equal([3, 6, 9]);
+  expect(twenty()).to.deep.equal([3, 6, 9, 12, 15, 18]);
+  expect(fifty()).to.deep.equal([3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48]);
 });
-
-
-function ten(){
-  var count = 0;
-  var values = "";
-  for(var i = 0; i < 10; i ++) {
-    if (i % 3 === 0) {
-      values += (i);
-    }
-  }
-  return values.split("");
-};
-
-function twenty(){
-  var count = 0;
-  var values = "";
-  for(var i = 0; i < 20; i ++) {
-    if (i % 3 === 0) {
-      values += (i);
-    }
-  }
-  return values.split(" "); // splits 12 into '1', '2'
-};
-
-function fifty(){
-  var count = 0;
-  var values = "";
-  for(var i = 0; i < 50; i ++) {
-    if (i % 3 === 0) {
-      values += (i);
-    }
-  }
-  return values.split(""); //TODO figure out how NOT to split double digits.
-};
 
 // END test 3s
+
 test('getting a list of multiples of 5', function () {
-  expect(ten()).to.be("0","5");
-  expect(twenty()).to.be("0","5", "10", "15");
-  expect(fifty()).to.be("0", "5", "10", "15", "20", "25", "30", "35", "40", "45");
+  expect(ten5()).to.deep.equal([5]);
+  expect(twenty5()).to.deep.equal([5, 10, 15]);
+  expect(fifty5()).to.deep.equal([5, 10, 15, 20, 25, 30, 35, 40, 45]);
 });
 
-function ten(){
-  var count = 0;
-  var values = "";
-  for(var i = 0; i < 10; i ++) {
-    if (i % 5 === 0) {
-      values += (i);
-    }
-  }
-  return values.split("");
+function ten5(){
+  return multiples(5, 10);
 };
 
-function twenty(){ //TODO make this iterate from 10 and set the split to 2 digits. Repeat for 50.
-  var count = 0;
-  var values = "";
-  for(var i = 0; i < 20; i ++) {
-    if (i % 5 === 0) {
-      values += (i);
-    }
-  }
-  return values.split(" "); 
+function twenty5(){
+  return multiples(5, 20);
 };
 
-function fifty(){
-  var count = 0;
-  var values = "";
-  for(var i = 0; i < 50; i ++) {
-    if (i % 5 === 0) {
-      values += (i);
-    }
-  }
-  return values.split("");
+function fifty5(){
+  return multiples(5, 50);
 };
