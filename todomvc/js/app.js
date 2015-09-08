178508d78dbb3606464f1e109e08733b59dcf67f
Lacey Rice <Ricecoder@users.noreply.github.com>
2015-09-08 17:13:57 -0400

    Added code comments
    Add two comments explaining what I think the code is saying.

--- todomvc/js/app.js
+++ todomvc/js/app.js
@@ -1,4 +1,5 @@
-(function (window) {
+(function (window) { //this function takes window object which represents a window containing the DOM document. 
+		     //I believe the function(window) is saying that in this function the code will be part of the DOM document and be displayed in the browser as part of the page.  	
 	'use strict';
 
 	// Your starting point. Enjoy the ride!
