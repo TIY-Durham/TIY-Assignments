6cbf43cfd6be1c36d0c320b04339b3879dc4b1b1
Lacey R <laceyrice@me.com>
2015-09-08 19:31:59 -0400

    first changes made

--- APIGymnastics/Etsy.js
+++ APIGymnastics/Etsy.js
@@ -1,17 +1,27 @@
 // Put `items.json` in your `APIGymnastics` directory...
 var items = require('items.json');
+    test = require('mocha').it,
+    expect = require('chai').expect;
 
 // TODO: Setup mocha and chai...
+var items = require('items.json');
+
 
 test('this is the easy one', function(){
   expect(Array.isArray(items)).to.be.true; // What.
   // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray
 
-  expect(items.length).to.equal(FILL_ME_IN); // It's haunting me now...
+  expect(items.length).to.equal(25); // It's haunting me now...
 });
+/*function length (items){
+  var items = [];
+  return items.length;
+
+}*/
 
 test('finding the average price', function(){
   var yourAnswer = "start with `items`; use `Array` methods";
+  items.concat
 
   expect(yourAnswer).to.be.closeTo(23.63, 0.01);
 });
