ef4f6dc6e68736f7692b14f767c2152d2a5045b4
Lacey R <laceyrice@me.com>
2015-09-09 19:44:19 -0400

    made changes I guess

--- NestedAccordion/js/scripts.js
+++ NestedAccordion/js/scripts.js
@@ -6,4 +6,5 @@
  On this page:
 * JavaScript adds the class "cbp-ntopen" to the li element when it is clicked. This allows the element to
   "accordion" out and show more text. The accordion function is the javascript which is responsible for adding and
-  removing the class if it is clicked on while open. 
+  removing the class if it is clicked on while open.
+  
