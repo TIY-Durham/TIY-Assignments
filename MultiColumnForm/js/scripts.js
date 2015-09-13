e2f17daa0a6dc7be247c4345b11d713f1e517167
Lacey R <laceyrice@me.com>
2015-09-13 12:53:07 -0400

    added more psuedocode

--- MultiColumnForm/js/scripts.js
+++ MultiColumnForm/js/scripts.js
@@ -1,6 +1,16 @@
 /*
 1.) Make variables for all of the elements I will be manipulating
+    a. start with the form element and one type with in
+    b. make variable for one of each type of form elements
+    c. make them all!
 
 2.) Make functions for every(maybe) form element.
+    a. make a function that will console.log for the form
+    b. make a function for one
+    c. then make function for one of each type of form element
+    d. then make them all! muwahahaha
 
-3.) Make eventListeners 
+
+3.) Make eventListeners
+    a. make eventListener for form
+    b. make eventListeners as needed
